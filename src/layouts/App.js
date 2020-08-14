83116ff5b075a6eb385b5c2e34c3a54699498e49
Mahmoud Hammad <hodaman2012@gmail.com>
2020-08-14 16:40:56 +0200

    temp fix

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,49 +1,36 @@
 import React from "react";
 import "./App.css";
 import { Cat } from "../components/Cat";
-import CustomInput from "../components/Form/CustomInput"
+import CustomInput from "../components/Form/CustomInput";
 // @material-ui/core components
 import Navbar from "../components/Navbar/Navbar.js";
 
+function renderlol() {
+  let tot = [];
+  for (let i = 0; i < 100; i++) {
+    tot.push(<h1>hey lol lol how are you lolololo lol lollo ooolo </h1>);
+  }
+  return tot;
+}
 export default function Admin() {
+  const  listenToScroll = (e) => {
+  console.log(e)
+  };
   return (
-    <div >
+    <div>
       <Navbar color="info" />
       <div className="content">
         <Cat />
 
-        <div className="addheight">
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-            <h1> hiahfa fjwaij oi fhawiehfo</h1>
-          </div>
+        <div onScroll={(e) => console.log("WOOWOWOW", e)} className="addheight">
+          {renderlol()}
+        </div>
 
-          
         <div className="headerContainer">
           <div className="brandContainer">
-          <h2 className="brand">PI</h2>
+            <h2 className="brand">PI</h2>
             <p className="glow slogan">Enlighten Your Way</p>
-
           </div>
-          
         </div>
       </div>
     </div>
