2cb39d963fa904590e9168705ec0feb3a60aa505
Mahmoud Hammad <hodaman2012@gmail.com>
2020-09-10 21:27:07 +0200

    events styling

--- src/Components/Content/Events/index.js
+++ src/Components/Content/Events/index.js
@@ -1,8 +1,66 @@
 import React from "react";
+import "./index.css";
 export default function Events() {
   return (
-    <div>
-      <h1>this is events</h1>
+    <div className="slider">
+      <div className="slides">
+
+        <div name="slide-1">
+          <div className="eventsM">
+            <h2>Pi Insights</h2>
+            <hr />
+            <div className="row">
+              <div className="col-md-6">
+                <h4>Our Vision</h4>
+                <p>
+                  Our vision is upgrading the students scientifically through
+                  applying innovation, intellectually through cultural awareness
+                </p>
+              </div>
+              <div className="col-md-6">
+                <h4>Our Mission</h4>
+                <p>
+                  To increase awareness and knowledge in science, research, and
+                  technology
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+        <div name="slide-2">
+          <div className="eventsM">
+            <h1>Event 2</h1>
+            <hr />
+            <div className="row">
+              <div className="col-md-6">
+                <h4>Our Vision</h4>
+                <p>
+                  Our vision is upgrading the students scientifically through
+                  applying innovation, intellectually through cultural awareness
+                </p>
+                <p>
+                  Our vision is upgrading the students scientifically through
+                  applying innovation, intellectually through cultural awareness
+                </p>
+                <p>
+                  Our vision is upgrading the students scientifically through
+                  applying innovation, intellectually through cultural awareness
+                </p>
+              </div>
+              <div className="col-md-6">
+                <h4>Our Mission</h4>
+                <p>
+                  To increase awareness and knowledge in science, research, and
+                  technology
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+      </div>
     </div>
   );
 }
